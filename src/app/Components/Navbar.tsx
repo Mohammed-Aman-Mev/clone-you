22f7191027d190a8a7ecde7f3313d34049de453e
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-01-12 12:26:11 +0530

    Navbar styling

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,11 +1,18 @@
+import { FaPlay } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
 import { IoIosSearch, IoMdNotificationsOutline } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-black sm:h-[56px] px-[16px] flex justify-between items-center">
-      <div className="h-full border-2 border-gray-500">
-        <FiMenu />
+    <nav className="w-full bg-black h-[56px] px-[8px] sm:px-[16px] flex justify-between items-center">
+      <div className="h-full flex justify-between items-center w-[140px]">
+        <FiMenu className="text-[25px]"/>
+        <div className="flex items-center w-[90px] justify-between">
+          <div className="bg-[#ff1111] w-[35px] h-[22px] dicen rounded-md">
+            <FaPlay className="bg-[#ff1111] text-[10px]" />
+          </div>
+          <h3 className="font-semibold">UTube</h3>
+        </div>
       </div>
       <div className="h-full border-2 border-gray-500">
         <IoIosSearch />
